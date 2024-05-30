92cb93cb631fad79b3a12a5dbc53f216f16da2f8
Danny <bermudd95@icloud.com>
2024-05-30 08:29:22 -0700

    added firebase file to gitignore

---
+++ FirebaseConfigSecret.js
@@ -0,0 +1,10 @@
+const FirebaseConfigSecret = {
+	authDomain: "arma-noire.firebaseapp.com",
+	projectId: "arma-noire",
+	storageBucket: "arma-noire.appspot.com",
+	messagingSenderId: "420027921308",
+	appId: "1:420027921308:web:41b0f05136f7aaee394e59",
+	measurementId: "G-E1CXEW7V1H",
+};
+
+export default FirebaseConfigSecret;
